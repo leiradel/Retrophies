1228888d213cb88238c9631d9e390b1a94a3bd69
leiradel <andre@leiradella.com>
2017-09-12 20:58:37 +0100

    Added the lexer

---
+++ compiler/lexer.c
@@ -0,0 +1,370 @@
+#include "lexer.h"
+#include "tokens.h"
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+#define RETROPHIES_LEXER_ISSPACE(k)  (isspace(k))
+#define RETROPHIES_LEXER_ISALPHA(k)  (k == '_' || isalpha(k))
+#define RETROPHIES_LEXER_ISALNUM(k)  (k == '_' || isalnum(k))
+#define RETROPHIES_LEXER_ISDIGIT(k)  (isdigit(k))
+#define RETROPHIES_LEXER_ISXDIGIT(k) (isxdigit(k))
+#define RETROPHIES_LEXER_ISBDIGIT(k) ((k & 0xfe) == '0' /* ascii only */)
+
+#define RETROPHIES_LEXER_GET(s) ((s)->source < (s)->end ? *(s)->source : -1)
+
+void retrophies_lexer_init(retrophies_lexer_t* self, const void* source, size_t length, char* error, size_t error_size)
+{
+  self->source     = (uint8_t*)source;
+  self->end        = self->source + length;
+  self->last_char  = RETROPHIES_LEXER_GET(self);
+  self->line       = 0;
+  self->error      = error;
+  self->error_size = error_size;
+}
+
+static inline int retrophies_lexer_skip(retrophies_lexer_t* self)
+{
+  self->line += *self->source == '\n';
+
+  ptrdiff_t left = self->end - self->source;
+
+  if (left >= 2)
+  {
+    return *++self->source;
+  }
+  else if (left == 1)
+  {
+    self->source++;
+  }
+
+  return -1;
+}
+
+static void retrophies_lexer_formatchar(char* buffer, size_t size, int k)
+{
+  if (isprint(k))
+  {
+    snprintf(buffer, size, "'%c'", k);
+  }
+  else if (k != -1)
+  {
+    snprintf(buffer, size, "'\\%c%c%c", (k >> 6) + '0', ((k >> 3) & 7) + '0', (k & 7) + '0');
+  }
+  else
+  {
+    strncpy(buffer, "<eof>", size);
+  }
+}
+
+static int retrophies_lexer_error(retrophies_lexer_t* self, const char* format, ...)
+{
+  va_list args;
+
+  va_start(args, format);
+  vsnprintf(self->error, self->error_size, format, args);
+  va_end(args);
+
+  return -1;
+}
+
+int retrophies_lexer_next(retrophies_lexer_t* self, retrophies_lexer_lookahead_t* la)
+{
+  int k = self->last_char;
+
+  for (;;)
+  {
+    if (k == -1)
+    {
+      la->token = RETROPHIES_TOKEN_EOF;
+      la->lexeme.chars = "<eof>";
+      la->lexeme.length = 5;
+      la->line = self->line;
+    }
+    else if (!RETROPHIES_LEXER_ISSPACE(k))
+    {
+      break;
+    }
+
+    k = retrophies_lexer_skip(self);
+  }
+
+  const uint8_t* start = self->source;
+  la->lexeme.chars     = (const char*)start;
+  la->line             = self->line;
+
+  if (RETROPHIES_LEXER_ISALPHA(k))
+  {
+    do
+    {
+      k = retrophies_lexer_skip(self);
+    }
+    while (RETROPHIES_LEXER_ISALPHA(k));
+
+    size_t length = self->source - start;
+    const struct retrophies_lexer_keyword_t* keyword = retrophies_lexer_lookup(la->lexeme.chars, length);
+
+    la->token         = (keyword != NULL) ? keyword->token : RETROPHIES_TOKEN_IDENTIFIER;
+    la->lexeme.length = length;
+    self->last_char   = k;
+    return 0;
+  }
+
+  if (RETROPHIES_LEXER_ISDIGIT(k))
+  {
+    int isint = 1;
+
+    do
+    {
+      k = retrophies_lexer_skip(self);
+    }
+    while (RETROPHIES_LEXER_ISDIGIT(k));
+
+    if (k == '.')
+    {
+      isint = 0;
+      k = retrophies_lexer_skip(self);
+
+      if (RETROPHIES_LEXER_ISDIGIT(k))
+      {
+        do
+        {
+          k = retrophies_lexer_skip(self);
+        }
+        while (RETROPHIES_LEXER_ISDIGIT(k));
+      }
+      else
+      {
+        char c[8];
+
+        retrophies_lexer_formatchar(c, sizeof(c), k);
+        return retrophies_lexer_error(self, "Invalid digit after decimal separator: %c", c);
+      }
+    }
+
+    if ((k & 0xdf) == 'E') /* ascii only */
+    {
+      isint = 0;
+      k = retrophies_lexer_skip(self);
+
+      if (k == '+' || k == '-')
+      {
+        k = retrophies_lexer_skip(self);
+      }
+
+      if (RETROPHIES_LEXER_ISDIGIT(k))
+      {
+        do
+        {
+          k = retrophies_lexer_skip(self);
+        }
+        while (RETROPHIES_LEXER_ISDIGIT(k));
+      }
+      else
+      {
+        char c[8];
+
+        retrophies_lexer_formatchar(c, sizeof(c), k);
+        return retrophies_lexer_error(self, "Invalid digit after decimal separator: %c", c);
+      }
+    }
+
+    const uint8_t* suffix_start = self->source - 1;
+    uint32_t suffix = 0;
+    int suffix_length;
+    
+    if (RETROPHIES_LEXER_ISALPHA(k))
+    {
+      do
+      {
+        suffix = suffix << 8 | tolower(k);
+        k = retrophies_lexer_skip(self);
+      }
+      while (RETROPHIES_LEXER_ISALPHA(k));
+
+      if (isint)
+      {
+        switch (suffix)
+        {
+        case 0:
+          suffix_length = 0;
+          break;
+        
+        case 's': /* short */
+        case 'i': /* integer */
+        case 'l': /* long */
+          suffix_length = 1;
+          break;
+
+        case 'u' << 8 | 's': /* unsigned short */
+        case 'u' << 8 | 'i': /* unsigned integer */
+        case 'u' << 8 | 'l': /* unsigned long */
+          suffix_length = 2;
+          break;
+
+        default:
+          return retrophies_lexer_error(self, "Invalid integer suffix: %.*s", suffix_start, self->source - suffix_start);
+        }
+      }
+      else
+      {
+        switch (suffix)
+        {
+        case 0:
+          suffix_length = 0;
+          break;
+
+        case 'f': /* single */
+        case 'r': /* double */
+          suffix_length = 1;
+          break;
+        
+        default:
+          return retrophies_lexer_error(self, "Invalid float suffix: %.*s", suffix_start, self->source - suffix_start);
+        }
+      }
+    }
+
+    la->token         = isint ? RETROPHIES_TOKEN_INTEGER_LITERAL : RETROPHIES_TOKEN_FLOAT_LITERAL;
+    la->lexeme.length = self->source - start - suffix_length;
+    self->last_char   = k;
+    return 0;
+  }
+
+  if (k == '"')
+  {
+    for (;;)
+    {
+      k = retrophies_lexer_skip(self);
+
+      if (k == '"')
+      {
+        if (RETROPHIES_LEXER_GET(self) == '"')
+        {
+          k = retrophies_lexer_skip(self);
+        }
+        else
+        {
+          break;
+        }
+      }
+      else if (k == '"' || k == -1)
+      {
+        return retrophies_lexer_error(self, "Unterminated string");
+      }
+    }
+
+    la->token         = RETROPHIES_TOKEN_STRING_LITERAL;
+    la->lexeme.length = self->source - start;
+    self->last_char   = k;
+    return 0;
+  }
+
+  int l;
+
+  switch (k)
+  {
+  case '(':
+  case ')':
+  case ',':
+  case '=':
+  case '.':
+  case '[':
+  case ']':
+  case '&':
+  case '+':
+  case '-':
+  case '*':
+  case '/':
+  case '\\':
+    la->token         = k;
+    la->lexeme.length = 1;
+    self->last_char   = retrophies_lexer_skip(self);
+    return 0;
+  
+  case '<':
+    l = k;
+    k = retrophies_lexer_skip(self);
+
+    if (k == '>')
+    {
+      la->token         = RETROPHIES_TOKEN_NOTEQUAL;
+      la->lexeme.length = 2;
+      self->last_char   = retrophies_lexer_skip(self);
+    }
+    else if (k == '=')
+    {
+      la->token         = RETROPHIES_TOKEN_LESSEQUAL;
+      la->lexeme.length = 2;
+      self->last_char   = retrophies_lexer_skip(self);
+    }
+    else if (k == '<')
+    {
+      la->token         = RETROPHIES_TOKEN_LEFTSHIFT;
+      la->lexeme.length = 2;
+      self->last_char   = retrophies_lexer_skip(self);
+    }
+    else
+    {
+      la->token         = l;
+      la->lexeme.length = 1;
+      self->last_char   = k;
+    }
+
+    return 0;
+
+  case '>':
+    l = k;
+    k = retrophies_lexer_skip(self);
+
+    if (k == '=')
+    {
+      la->token         = RETROPHIES_TOKEN_GREATEREQUAL;
+      la->lexeme.length = 2;
+      self->last_char   = retrophies_lexer_skip(self);
+    }
+    else if (k == '>')
+    {
+      la->token         = RETROPHIES_TOKEN_RIGHTSHIFT;
+      la->lexeme.length = 2;
+      self->last_char   = retrophies_lexer_skip(self);
+    }
+    else
+    {
+      la->token         = l;
+      la->lexeme.length = 1;
+      self->last_char   = k;
+    }
+
+    return 0;
+  }
+
+  char c[8];
+  
+  retrophies_lexer_formatchar(c, sizeof(c), k);
+  return retrophies_lexer_error(self, "Invalid character in input: %c", c);
+}
+
+void retrophies_lexer_getlexeme(char* buffer, size_t size, int token)
+{
+  static const char* lexemes[] =
+  {
+    "<eof>", "false", "<id>", "<integer>", "<float>", "<string>", "true", "else", "elseif",
+    "end", "for", "if", "next", "repeat", "step", "then", "to", "until", "while", "as",
+    "dim", "function", "static", "sub", "randomize", "return", "abs", "ceil", "floor", "rand",
+    "trunc", "boolean", "double", "int8", "int16", "int32", "int64", "single", "string",
+    "uint8", "uint16", "uint32", "uint64", "and", ">=", "<<", "<=", "mod", "not", "<>", "or",
+    ">>", "xor", "bigendian", "binarycodeddecimal", "littleendian"
+  };
+
+  if (token >= RETROPHIES_TOKEN_EOF && token < RETROPHIES_TOKEN_LAST)
+  {
+    strncpy(buffer, lexemes[token - RETROPHIES_TOKEN_EOF], size);
+  }
+  else
+  {
+    retrophies_lexer_formatchar(buffer, size, token);
+  }
+}
---
+++ compiler/lexer.h
@@ -0,0 +1,117 @@
+#ifndef RETROPHIES_LEXER_H
+#define RETROPHIES_LEXER_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+enum
+{
+  RETROPHIES_TOKEN_EOF = 256,
+
+  /* Literals */
+  RETROPHIES_TOKEN_FALSE,
+  RETROPHIES_TOKEN_IDENTIFIER,
+  RETROPHIES_TOKEN_INTEGER_LITERAL,
+  RETROPHIES_TOKEN_FLOAT_LITERAL,
+  RETROPHIES_TOKEN_STRING_LITERAL,
+  RETROPHIES_TOKEN_TRUE,
+
+  /* Control-flow */
+  RETROPHIES_TOKEN_ELSE,
+  RETROPHIES_TOKEN_ELSEIF,
+  RETROPHIES_TOKEN_END,
+  RETROPHIES_TOKEN_FOR,
+  RETROPHIES_TOKEN_IF,
+  RETROPHIES_TOKEN_NEXT,
+  RETROPHIES_TOKEN_REPEAT,
+  RETROPHIES_TOKEN_STEP,
+  RETROPHIES_TOKEN_THEN,
+  RETROPHIES_TOKEN_TO,
+  RETROPHIES_TOKEN_UNTIL,
+  RETROPHIES_TOKEN_WHILE,
+
+  /* Declarations */
+  RETROPHIES_TOKEN_AS,
+  RETROPHIES_TOKEN_DIM,
+  RETROPHIES_TOKEN_FUNCTION,
+  RETROPHIES_TOKEN_HANDLES,
+  RETROPHIES_TOKEN_STATIC,
+  RETROPHIES_TOKEN_SUB,
+  RETROPHIES_TOKEN_TROPHY,
+  
+  /* Commands */
+  RETROPHIES_TOKEN_RANDOMIZE,
+  RETROPHIES_TOKEN_RETURN,
+
+  /* Functions */
+  RETROPHIES_TOKEN_ABS,
+  RETROPHIES_TOKEN_CEIL,
+  RETROPHIES_TOKEN_FLOOR,
+  RETROPHIES_TOKEN_RAND,
+  RETROPHIES_TOKEN_TRUNC,
+
+  /* Types */
+  RETROPHIES_TOKEN_BOOLEAN,
+  RETROPHIES_TOKEN_DOUBLE,
+  RETROPHIES_TOKEN_INT8,
+  RETROPHIES_TOKEN_INT16,
+  RETROPHIES_TOKEN_INT32,
+  RETROPHIES_TOKEN_INT64,
+  RETROPHIES_TOKEN_SINGLE,
+  RETROPHIES_TOKEN_STRING,
+  RETROPHIES_TOKEN_UINT8,
+  RETROPHIES_TOKEN_UINT16,
+  RETROPHIES_TOKEN_UINT32,
+  RETROPHIES_TOKEN_UINT64,
+
+  /* Operators */
+  RETROPHIES_TOKEN_AND,
+  RETROPHIES_TOKEN_GREATEREQUAL,
+  RETROPHIES_TOKEN_LEFTSHIFT,
+  RETROPHIES_TOKEN_LESSEQUAL,
+  RETROPHIES_TOKEN_MOD,
+  RETROPHIES_TOKEN_NOT,
+  RETROPHIES_TOKEN_NOTEQUAL,
+  RETROPHIES_TOKEN_OR,
+  RETROPHIES_TOKEN_RIGHTSHIFT,
+  RETROPHIES_TOKEN_XOR,
+
+  /* Memory */
+  RETROPHIES_TOKEN_BIGENDIAN,
+  RETROPHIES_TOKEN_BINARYCODEDDECIMAL,
+  RETROPHIES_TOKEN_LITTLEENDIAN,
+
+  RETROPHIES_TOKEN_LAST
+};
+
+typedef struct
+{
+  const char* chars;
+  size_t      length;
+}
+retrophies_lexer_string_t;
+
+typedef struct
+{
+  int                       token;
+  retrophies_lexer_string_t lexeme;
+  unsigned                  line;
+}
+retrophies_lexer_lookahead_t;
+
+typedef struct
+{
+  const uint8_t* source;
+  const uint8_t* end;
+  int            last_char;
+  unsigned       line;
+  char*          error;
+  size_t         error_size;
+}
+retrophies_lexer_t;
+
+void retrophies_lexer_init(retrophies_lexer_t* self, const void* source, size_t length, char* error, size_t error_size);
+int  retrophies_lexer_next(retrophies_lexer_t* self, retrophies_lexer_lookahead_t* la);
+void retrophies_lexer_getlexeme(char* buffer, size_t size, int token);
+
+#endif /* RETROPHIES_LEXER_H */
